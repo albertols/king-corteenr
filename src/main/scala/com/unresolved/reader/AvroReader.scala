4e403efe9461afb8887e0bb2067805ea2368db91
albertols <albertols87@gmail.com>
2023-02-24 17:40:47 +0100

    welcome avro

--- src/main/scala/com/unresolved/reader/AvroReader.scala
+++ src/main/scala/com/unresolved/reader/AvroReader.scala
@@ -2,7 +2,7 @@ package com.unresolved.reader
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-case class AvroReader(options: Map[String, String]) extends AbstractReaderDf {
+case class AvroReader(options: Map[String, String]  = Map()) extends AbstractReaderDf {
 
   override def readDf(path: String)(implicit spark: SparkSession): DataFrame = spark
     .read
